86cd28a3c64b2265f52d7baeb39c35f2cdbae7f6
Andy Rufasto <andy@andyrufasto.cf>
2020-12-22 15:11:50 -0500

    postgrest SQL config added

--- config.php
+++ config.php
@@ -4,21 +4,21 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = getenv('DATABASE_TYPE');
+$CFG->dbtype    = getenv('pgsql');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('DATABASE_HOST');
-$CFG->dbname    = getenv('DATABASE_NAME');
-$CFG->dbuser    = getenv('DATABASE_USER');
-$CFG->dbpass    = getenv('DATABASE_PASSWORD');
+$CFG->dbhost    = getenv('ec2-18-211-171-122.compute-1.amazonaws.com');
+$CFG->dbname    = getenv('dbuklg11sb8s3f');
+$CFG->dbuser    = getenv('juawqjlpqtjeyv');
+$CFG->dbpass    = getenv('3b2a2c0d5b03fb0c703d82915b3ec1b74ef2bf829549e6e3ecab6e47eb7b0404');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => getenv('DATABASE_PORT'),
+  'dbport' => getenv('5432'),
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = getenv('WWWROOT');
-$CFG->dataroot  = getenv('DATAROOT');
+$CFG->wwwroot   = getenv('http://moodle-andyrufasto.herokuapp.com');
+$CFG->dataroot  = getenv('/tmp');
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
